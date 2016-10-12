81579f03e078a1b691a0278d785102aa25b018df
Jerry <jerryhanjj@126.com>
2016-10-12 19:11:37 +0800

    Update html_outputer.py

--- html_outputer.py
+++ html_outputer.py
@@ -9,7 +9,7 @@ class HtmlOutputer(object):
         self.datas.append(data)
 
     def output_html(self):
-        with open('C:/Users/Administrator/Desktop/output.html', 'w',encoding='utf-8', errors='ignore')as fout:
+        with open('output.html', 'w',encoding='utf-8', errors='ignore')as fout:
             fout.write("<html>")
             fout.write('<head><meta http-equiv="content-type" content="text/html;charset=utf-8"></head>')
             fout.write("<body>")
